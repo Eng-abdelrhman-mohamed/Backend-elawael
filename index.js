2c1e2794cc99521f8baba87ddd13897b7f0362fe
Eng-abdelrhman-mohamed <157839745+Eng-abdelrhman-mohamed@users.noreply.github.com>
2025-01-29 13:15:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express();
 const TeacherRoute = require('./routes/Teachers.route');
 const CommunityRoute = require('./routes/Community.route');
 const UserRoute = require('./routes/Users.route');
+const BooksRoute = require('./routes/Books.route');
+const NotebookRoute = require('./routes/Notebook.route');
+const starRoute = require('./routes/Notebook.route');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
@@ -25,6 +28,9 @@ app.use(express.json());
 app.use('/teachers',TeacherRoute);
 app.use('/communities',CommunityRoute);
 app.use('/user',UserRoute);
+app.use('/books',BooksRoute);
+app.use('/notebook',NotebookRoute);
+app.use('/star',starRoute);
 
 
 // app
